a255360d7b8dfcbf416e62306b3f374b1191a2ba
Maxime ANDRE <andremaxime7@gmail.com>
2022-05-18 17:00:24 +0200

    restaurant and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number: '0123456789', category: 'chinese' }
+pizza = { name: 'Pizza', address: '56A Shore High St, London E1 6PQ', phone_number: '0123456789', category: 'italian' }
+surpriz = { name: 'Surpriz', address: 'rue Oberkampf', phone_number: '0123456789', category: 'belgian' }
+brasserie = { name: 'Brasserie', address: 'rue du banquier', phone_number: '0123456789', category: 'french' }
+sushi = { name: 'Sushi', address: 'rue de Tokyo', phone_number: '0123456789', category: 'japanese' }
+
+[dishoom, pizza, surpriz, brasserie, sushi].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts 'Finished!'
